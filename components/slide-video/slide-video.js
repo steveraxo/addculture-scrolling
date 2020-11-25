fe7fccd1c1dfb955bb96f5fac44848d9b94eb278
steveraxo <steve@raxo.tv>
2020-11-25 10:35:05 -0400

    added mute to video

--- components/slide-video/slide-video.js
+++ components/slide-video/slide-video.js
@@ -33,7 +33,7 @@ export default class SlideVideo extends Component {
             <>
                 <div id="set-height"></div>
                 <p id="time"></p>
-                <video id="v0" autoPlay tabIndex="0" autobuffer="autobuffer" preload="preload"><source type="video/webm; codecs=&quot;vp8, vorbis&quot;" src="https://rx.raxo.dev/wp-content/uploads/2020/06/RxSite_HOME_Loop2.webm"></source>
+                <video id="v0" autoPlay muted tabIndex="0" autobuffer="autobuffer" preload="preload"><source type="video/webm; codecs=&quot;vp8, vorbis&quot;" src="https://rx.raxo.dev/wp-content/uploads/2020/06/RxSite_HOME_Loop2.webm"></source>
                 <source type="video/ogg; codecs=&quot;theora, vorbis&quot;" src="https://rx.raxo.dev/wp-content/uploads/2020/06/RxSite_HOME_Loop2.ogv"></source>
                 <source type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" src="https://rx.raxo.dev/wp-content/uploads/2020/06/RxSite_HOME_Loop2.mp4"></source>
                 <p>Sorry, your browser does not support the &lt;video&gt; element.</p>

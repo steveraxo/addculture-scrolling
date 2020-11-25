75e9770033bb3e3cf82854192116390850f73342
steveraxo <steve@raxo.tv>
2020-11-25 16:24:17 -0400

    added smooth scroll and section snapping

--- components/mobile-version/index.js
+++ components/mobile-version/index.js
@@ -35,6 +35,8 @@ export default class MobileIndex extends Component {
                 mobile: true,
             });
         }
+
+        
     }
 
     switchChart(event){
@@ -81,7 +83,79 @@ export default class MobileIndex extends Component {
         };
         
         return (
-            <>
+            <div id="scroll-container">
+                <script
+                    src="https://code.jquery.com/jquery-3.5.1.min.js"
+                    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+                    crossorigin="anonymous">
+                </script>
+                <script>
+                    {`
+                        // Smooth scroll
+                        if ($('#scroll-container').length > 0) {
+                            var html = document.documentElement;
+                            var body = document.body;
+                            var scroller = {
+                            target: document.querySelector("#scroll-container"),
+                            ease: 0.09, // <= scroll speed
+                            endY: 0,
+                            y: 0,
+                            resizeRequest: 1,
+                            scrollRequest: 0,
+                            };
+                            var requestId = null;
+                            TweenLite.set(scroller.target, {
+                            rotation: 0.01,
+                            force3D: true
+                            });
+                            window.addEventListener("load", onLoad);
+                            function onLoad() {
+                            updateScroller();
+                            window.focus();
+                            window.addEventListener("resize", onResize);
+                            document.addEventListener("scroll", onScroll);
+                            }
+                            function updateScroller() {
+                            var resized = scroller.resizeRequest > 0;
+                            if (resized) {
+                                var height = scroller.target.clientHeight;
+                                body.style.height = height + "px";
+                                scroller.resizeRequest = 0;
+                            }
+                            var scrollY = window.page__bookYOffset || html.scrollTop || body.scrollTop || 0;
+                            scroller.endY = scrollY;
+                            scroller.y += (scrollY - scroller.y) * scroller.ease;
+                            if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+                                scroller.y = scrollY;
+                                scroller.scrollRequest = 0;
+                            }
+                            TweenLite.set(scroller.target, {
+                                y: -scroller.y
+                            });
+                            requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+                            if($('#trending_title')){
+                                if($(window).scrollTop() + $(window).height() == $(document).height()) {
+                                    var height = scroller.target.clientHeight;
+                                    body.style.height = height + "px";
+                                    scroller.resizeRequest = 0;
+                                }
+                            }
+                            }
+                            function onScroll() {
+                            scroller.scrollRequest++;
+                            if (!requestId) {
+                                requestId = requestAnimationFrame(updateScroller);
+                            }
+                            }
+                            function onResize() {
+                            scroller.resizeRequest++;
+                            if (!requestId) {
+                                requestId = requestAnimationFrame(updateScroller);
+                            }
+                            }
+                        }
+                    `}
+                </script>
                 <section id="are-failing" className="mobile__slide">
                     <div className="floating__circle">
                         <img src="/images/plus.svg" data-aos="fade-left" data-aos-delay="100" data-aos-duration="1000"  alt=""/>
@@ -467,7 +541,7 @@ export default class MobileIndex extends Component {
                 <CtaSection />
 
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-grid-only@1.0.0/bootstrap.css"/>
-            </> 
+            </div> 
         )
     }
 }

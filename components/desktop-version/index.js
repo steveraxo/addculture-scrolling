269269020634f549d6195b9a736e713f9ff4f35a
steveraxo <steve@raxo.tv>
2020-12-14 10:04:33 -0400

    fix

--- components/desktop-version/index.js
+++ components/desktop-version/index.js
@@ -352,6 +352,7 @@ export default class MobileIndex extends Component {
         }
 
         if(destination.index === 14){
+            document.getElementById("fp-nav").classList.add("showing");
             document.querySelectorAll(".PlayButton")[0].classList.remove("hidden");
 
             this.setState({videoController: true})
